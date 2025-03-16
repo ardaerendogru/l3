f179596266a7d36062de003d0a24009a81d49bca
Arda Eren Dogru <ardaeren.dogru@studenti.polito.it>
2025-03-16 03:33:14 +0100

    Added mixed precision training.

--- train.py
+++ train.py
@@ -64,7 +64,7 @@ def train_epoch(model, dataloader, criterion, optimizer, device, scaler, use_amp
         # to improve performance (increase speed and reduce vram usage ) while maintaining accuracy. The device_type is set to 'cuda' to indicate that
         # this is for GPU usage. The 'enabled' flag is set according to the config file, determining whether
         # mixed precision is used or not.
-        with torch.amp.autocast(device_type=device, enabled=use_amp):
+        with torch.amp.autocast(device_type=str(device), enabled=use_amp):
             outputs = model(inputs)
             loss = criterion(outputs, targets)
         
